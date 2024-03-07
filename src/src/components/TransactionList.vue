3bd9e20230578690c4676670b03db5483c563da9
google85 <bpfcomp2005@gmail.com>
2024-03-08 01:18:30 +0200

    refactor using setup()

--- src/src/components/TransactionList.vue
+++ src/src/components/TransactionList.vue
@@ -12,16 +12,17 @@
 
 <script>
   export default {
-    data() {
-      return {
-        transactions: [
+    setup() {
+      const transactions = [
           { id: 1, text: 'Flower', amount: -19.99 },
           { id: 2, text: 'Salary', amount: 299.97 },
           { id: 3, text: 'Book',   amount: -10 },
           { id: 4, text: 'Camera', amount: 150 },
-        ]
+        ];
+
+      return {
+        transactions
       }
     }
-
-  }
+  };
 </script>
