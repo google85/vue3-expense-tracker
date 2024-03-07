dcf1c9644e1b4cf626517c3d1e3d1857e3ea9385
google85 <bpfcomp2005@gmail.com>
2024-03-08 01:19:27 +0200

    refactor TransactionList component from 'options API' to 'script setup'

--- src/src/components/TransactionList.vue
+++ src/src/components/TransactionList.vue
@@ -10,19 +10,11 @@
   </ul>
 </template>
 
-<script>
-  export default {
-    setup() {
-      const transactions = [
-          { id: 1, text: 'Flower', amount: -19.99 },
-          { id: 2, text: 'Salary', amount: 299.97 },
-          { id: 3, text: 'Book',   amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 },
-        ];
-
-      return {
-        transactions
-      }
-    }
-  };
+<script setup>
+  const transactions = [
+      { id: 1, text: 'Flower', amount: -19.99 },
+      { id: 2, text: 'Salary', amount: 299.97 },
+      { id: 3, text: 'Book',   amount: -10 },
+      { id: 4, text: 'Camera', amount: 150 },
+    ];
 </script>
